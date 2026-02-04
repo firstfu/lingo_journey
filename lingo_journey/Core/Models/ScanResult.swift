57acdeb09dc3c6722a99715166033a2935117d7f
firstfu <firefirstfu@gmail.com>
2026-02-05 01:27:35 +0800

    feat(scanner): add translationFailed state to ScanResult

--- lingo_journey/Core/Models/ScanResult.swift
+++ lingo_journey/Core/Models/ScanResult.swift
@@ -7,18 +7,21 @@ struct ScanResult: Identifiable, Equatable {
     var translatedText: String?
     let boundingBox: CGRect
     var isTranslating: Bool
+    var translationFailed: Bool
 
     init(
         id: UUID = UUID(),
         originalText: String,
         translatedText: String? = nil,
         boundingBox: CGRect,
-        isTranslating: Bool = false
+        isTranslating: Bool = false,
+        translationFailed: Bool = false
     ) {
         self.id = id
         self.originalText = originalText
         self.translatedText = translatedText
         self.boundingBox = boundingBox
         self.isTranslating = isTranslating
+        self.translationFailed = translationFailed
     }
 }
